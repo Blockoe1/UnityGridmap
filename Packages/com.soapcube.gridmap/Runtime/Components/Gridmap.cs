8c8dac2d950748ec22a5afc66d106747bdfaf4aa
LucasFehlberg <lucas.fehlberg.17@gmail.com>
2025-12-19 21:44:06 -0600

    Add a function

--- Packages/com.soapcube.gridmap/Runtime/Components/Gridmap.cs
+++ Packages/com.soapcube.gridmap/Runtime/Components/Gridmap.cs
@@ -1,9 +1,18 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 namespace Gridmap
 {
-    public class Gridmap : MonoBehaviour
+    public class Gridmap : Component
     {
         private readonly Vector3Int chunkSize = new(16, 16, 16);
+
+        private List<MeshChunk> chunks = new List<MeshChunk>();
+
+        public void PlaceTileAtPoint(MeshTileBase tile, Vector3Int point)
+        {
+
+        }
     }
 }
